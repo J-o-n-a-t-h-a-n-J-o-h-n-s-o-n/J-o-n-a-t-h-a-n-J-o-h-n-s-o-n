19d1fcc7fd3a19c9da8517e6ba2ffa9b052569e8
Jonathan Johnson <107150758+J-o-n-a-t-h-a-n-J-o-h-n-s-o-n@users.noreply.github.com>
2023-03-07 14:30:05 +0000

    Update README.js

--- README.js
+++ README.js
@@ -1,7 +1,4 @@
 /*I’m interested in Javascript, CSS and HTML
 I’m currently learning Javascript*/
 
-<!---
-J-o-n-a-t-h-a-n-J-o-h-n-s-o-n/J-o-n-a-t-h-a-n-J-o-h-n-s-o-n is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+console.log("Prime is Good")
